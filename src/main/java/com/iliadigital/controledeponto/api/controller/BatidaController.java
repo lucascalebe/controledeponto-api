db64d19e3cfb97ea22dcf4c6adc942a1f376653d
Lucas Calebe <lucascalebe07@hotmail.com>
2021-11-13 21:03:36 -0300

    Correção response status para código 201

--- src/main/java/com/iliadigital/controledeponto/api/controller/BatidaController.java
+++ src/main/java/com/iliadigital/controledeponto/api/controller/BatidaController.java
@@ -1,11 +1,13 @@
 package com.iliadigital.controledeponto.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.iliadigital.controledeponto.domain.model.Momento;
@@ -19,8 +21,9 @@ public class BatidaController {
 	private BatidaService batidaService;
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Momento> baterPonto(@RequestBody Momento momento) {
 		Momento momentoSalvo = batidaService.baterPonto(momento);
-		return ResponseEntity.ok(momentoSalvo);
+		return ResponseEntity.status(HttpStatus.CREATED).body(momentoSalvo);
 	} 
 }
